0cdf29e265bbe1b7853047aea44b17678c430fd5
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-10-26 10:34:51 -0500

    added README

---
+++ README.md
@@ -0,0 +1,12 @@
+# JavaScript Drum Kit
+
+This project is a tutorial by Wes Bos via javascript30.com
+
+# How it works
+
+Press any of the keys shown to play the corresponding sound.
+
+# Tech
+- HTML
+- CSS
+- JavaScript
--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const key = document.getElementsByClassName('key')
 
-document.addEventListener("keydown", e=> {
-    console.log(e.which)
-    const audio = document.querySelector(`audio[data-key="${e.which}"]`);
-    const key = document.querySelector(`.key[data-key="${e.which}"]`)
+document.addEventListener("keydown", e => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    // if no audio exists for pressed key, return.
     if (!audio) return;
+    // rewind audio to 0 for multiple hits in a row
     audio.currentTime = 0;
+    // play the audio
     audio.play();
+    // add playing class for the animation
     key.classList.add('playing')
 })
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', e => {
+    if (e.propertyName !== 'transform') return;
+    // remove 'playing' class after css transition end
+    key.classList.remove('playing')
+}))
